5d5988ca94e02147daa195d26ca2ca4f3f4abf82
Gustavo Lima <76820333+gustavolimav@users.noreply.github.com>
2021-05-04 16:30:54 -0300

    Revert "Update test_run layout/layout-test"

--- sh/arquillian-search-tests-run-all.sh
+++ sh/arquillian-search-tests-run-all.sh
@@ -134,10 +134,7 @@ test_run dynamic-data-lists/dynamic-data-lists-test \
 	*.search.*Test
 
 test_run layout/layout-test \
-	*.search.*Test \
-	com.liferay.layout.search.test.LayoutIndexerReindexTest \
-	com.liferay.layout.search.test.LayoutRelevanceSearchTest \
-; \
+	*.search.*Test
 
 test_run message-boards/message-boards-test \
 	*.search.*Test \
